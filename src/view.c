eaf34fb77443fd9c84388f4c69228e6d864cc3a1
kickback <kickback@e2d53364-18fc-0310-981e-b60a84739af0>
2006-03-08 18:24:36 +0000

    sweep:view.c: fixed ticket:9 close while loading segfault. needed to cancel the active ops and remove a gtk_idle function in view_destroy_cb before removing the sample/window. comments in the cancel_active_op function mention possible leaks. i'll investigate that.
    git-svn-id: http://svn.metadecks.org/sweep/trunk@370 e2d53364-18fc-0310-981e-b60a84739af0

--- src/view.c
+++ src/view.c
@@ -38,6 +38,7 @@
 #include <sweep/sweep_types.h>
 #include <sweep/sweep_typeconvert.h>
 #include <sweep/sweep_sample.h>
+#include <sweep/sweep_undo.h>
 
 #include "view.h"
 
@@ -1506,6 +1507,9 @@ view_destroy_cb (GtkWidget * widget, gpointer data)
 
   sample_display_stop_marching_ants (SAMPLE_DISPLAY(view->display));
 
+  if (view->sample->op_progress_tag != -1)
+    gtk_timeout_remove(view->sample->op_progress_tag);
+  cancel_active_op (view->sample);
   sample_remove_view(view->sample, view);
   gtk_widget_destroy (GTK_WIDGET (view->display));
 
